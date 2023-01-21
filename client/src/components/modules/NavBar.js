ef62572f0a1c714da8bb34f7fd033cf311c19993
connieej <110947840+connieej@users.noreply.github.com>
2023-01-21 01:01:46 -0500

    fixed profile link

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -16,7 +16,7 @@ const NavBar = (props) => {
         </Link>
       </div>
       <div className="NavBar-linkContainer u-inlineBlock">
-        <Link to="/profile/" className="NavBar-link">
+        <Link to="/profile/:userId" className="NavBar-link">
           Profile
         </Link>
       </div>
