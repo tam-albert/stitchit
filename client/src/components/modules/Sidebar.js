abae421151e49e6ea0b4ce39a2e513beed1ded87
Albert Tam <tamalbert8@gmail.com>
2023-01-22 00:57:17 -0500

    add broen logout menu and fix tailwind

--- client/src/components/modules/Sidebar.js
+++ client/src/components/modules/Sidebar.js
@@ -1,4 +1,14 @@
 import React, { useState } from "react";
+import LogoutMenu from "./LogoutMenu";
+import {
+  PlusIcon,
+  BookOpenIcon,
+  ChatBubbleBottomCenterIcon,
+  DocumentIcon,
+  QueueListIcon,
+  UserCircleIcon,
+  ChevronLeftIcon,
+} from "@heroicons/react/24/outline";
 
 import "./Sidebar.css";
 
@@ -17,16 +27,7 @@ const Sidebar = (props) => {
               href="/"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg>
+              <PlusIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">New Stitch</span>
             </a>
           </li>
@@ -35,20 +36,7 @@ const Sidebar = (props) => {
               href="/myjournals"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-                />
-              </svg>
+              <BookOpenIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">My Journals</span>
             </a>
           </li>
@@ -57,20 +45,7 @@ const Sidebar = (props) => {
               href="/prompts"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-                />
-              </svg>
+              <ChatBubbleBottomCenterIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Prompts</span>
             </a>
           </li>
@@ -79,20 +54,7 @@ const Sidebar = (props) => {
               href="/drafts"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-                />
-              </svg>
+              <DocumentIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Drafts</span>
             </a>
           </li>
@@ -101,46 +63,21 @@ const Sidebar = (props) => {
               href="/feed"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
-                />
-              </svg>
+              <QueueListIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Feed</span>
             </a>
           </li>
         </ul>
         {props.userId ? (
-          <div className="bottom-0 px-2 py-4 text-lg font-normal text-gray-900 rounded-lg">
-            <a className="flex items-center" href={`/profile/${props.userId}`}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
+          <div className="bottom-0 px-2 py-4 text-lg font-normal text-gray-900 rounded-lg flex">
+            <a className="flex grow items-center" href={`/profile/${props.userId}`}>
+              <UserCircleIcon className="w-6 h-6" />
 
               <span className="flex-1 ml-3 truncate">
                 <span>{props.userName}</span>
               </span>
             </a>
+            <LogoutMenu />
           </div>
         ) : null}
         <button
@@ -150,16 +87,7 @@ const Sidebar = (props) => {
             setMinimized(1 - minimized);
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={3}
-            stroke="currentColor"
-            className="w-5 h-5"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
+          <ChevronLeftIcon className="w-5 h-5" />
         </button>
       </div>
     </aside>
