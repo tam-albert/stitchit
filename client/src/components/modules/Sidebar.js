4409645b769d21698b7d276eaef0ac0996c0ec5d
Albert Tam <tamalbert8@gmail.com>
2023-01-22 01:14:20 -0500

    lmao idk

--- client/src/components/modules/Sidebar.js
+++ client/src/components/modules/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "@reach/router";
 import LogoutMenu from "./LogoutMenu";
 import {
   PlusIcon,
@@ -23,60 +24,60 @@ const Sidebar = (props) => {
       <div className="px-3 py-4 overflow-y-auto flex flex-col rounded bg-gray-100 dark:bg-gray-800 h-full min-h-screen">
         <ul className="space-y-2 flex-grow">
           <li>
-            <a
-              href="/"
+            <Link
+              to="/"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
               <PlusIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">New Stitch</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/myjournals"
+            <Link
+              to="/myjournals"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
               <BookOpenIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">My Journals</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/prompts"
+            <Link
+              to="/prompts"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
               <ChatBubbleBottomCenterIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Prompts</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/drafts"
+            <Link
+              to="/drafts"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
               <DocumentIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Drafts</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/feed"
+            <Link
+              to="/feed"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             >
               <QueueListIcon className="w-6 h-6" />
               <span className="flex-1 ml-3 whitespace-nowrap">Feed</span>
-            </a>
+            </Link>
           </li>
         </ul>
         {props.userId ? (
           <div className="bottom-0 px-2 py-4 text-lg font-normal text-gray-900 rounded-lg flex">
-            <a className="flex grow items-center" href={`/profile/${props.userId}`}>
+            <Link className="flex grow items-center" to={`/profile/${props.userId}`}>
               <UserCircleIcon className="w-6 h-6" />
 
               <span className="flex-1 ml-3 truncate">
                 <span>{props.userName}</span>
               </span>
-            </a>
+            </Link>
             <LogoutMenu handleLogout={props.handleLogout} />
           </div>
         ) : null}
