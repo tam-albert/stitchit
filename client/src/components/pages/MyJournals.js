e1af449521b9c1f1026002efc760e63bb8402110
Albert Tam <tamalbert8@gmail.com>
2023-01-22 23:48:05 -0500

    add list of journals to myjournals page

--- client/src/components/pages/MyJournals.js
+++ client/src/components/pages/MyJournals.js
@@ -1,17 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NewJournal from "../modules/NewJournal";
+import { Link } from "@reach/router";
 
 import { get } from "../../utilities";
 
 const MyJournals = () => {
+  const [journals, setJournals] = useState([]);
+
   useEffect(() => {
-    get("/api/journals").then((journals) => {
-      console.log(journals);
+    get("/api/journals").then((journalObjs) => {
+      console.log(journalObjs);
+      setJournals(journalObjs);
     });
   }, []);
+
+  const journalsList = journals.map((journal) => (
+    <div>
+      <Link to={`/journal/${journal._id}`} key={journal._id}>
+        {journal.name}
+      </Link>
+    </div>
+  ));
+
+  console.log(journals);
+
   return (
     <>
       <NewJournal />
+      {journalsList}
     </>
   );
 };
