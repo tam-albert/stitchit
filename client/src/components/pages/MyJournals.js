7a736a163e33ef3657d305034ea37baea59ccc05
angelnuz <angelnuz@mit.edu>
2023-01-23 21:34:47 -0500

    changed css of my journals page

--- client/src/components/pages/MyJournals.js
+++ client/src/components/pages/MyJournals.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import NewJournal from "../modules/NewJournal";
 import { Link } from "@reach/router";
 
+import "./MyJournals.css"
 import { get } from "../../utilities";
 
 const MyJournals = () => {
@@ -15,7 +16,7 @@ const MyJournals = () => {
   }, []);
 
   const journalsList = journals.map((journal) => (
-    <div className="mz border-top-width-200 inline-flex border-2 border-gray-400 rounded-md p-4 my-4">
+    <div className="test border-top-width-200 inline-flex border-2 border-gray-400 rounded-md p-4 my-4">
       <Link to={`/journal/${journal._id}`} key={journal._id}>
         {journal.name} <span className="text-gray-500">by {journal.collaborator_names[0]}</span>
       </Link>
