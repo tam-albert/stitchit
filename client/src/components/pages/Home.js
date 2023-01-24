9050b82605c627968a8ac2c958f67e6a202ff1a6
Albert Tam <tamalbert8@gmail.com>
2023-01-23 23:31:14 -0500

    I LOVE EVERYBODY UNTIL I LOVE UUUU

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -8,12 +8,16 @@ const Home = (props) => {
 
   return (
     <>
-      <div className="px-4 h-full">
+      <div className="px-4 h-full flex flex-col">
         <textarea
-          className="w-full h-full p-4 resize-none rounded-md text-lg placeholder:italic placeholder:text-2xl"
+          className="w-full grow p-4 resize-none rounded-md text-lg placeholder:italic placeholder:text-2xl"
           placeholder="Speak your mind, add an image, or answer our daily prompt."
           maxlength="20000"
         ></textarea>
+        <div className="bg-light-pink my-1 p-4 rounded-md flex">
+          <div className="grow">HIIII</div>
+          <div>I LOVE EVERYBODY UNTIL I LOVE YOU</div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./client/src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#396dff",
+        "primary-dim": "#6987db",
+        secondary: "#41927d",
+        "secondary-dim": "#266858",
+        tertiary: "#a7d8bc",
+        "tertiary-dim": "#90bba4",
+        sunflower: "#ffd245",
+        "light-pink": "#fbcdc8",
+        "darker-pink": "#f79e8c",
+        darkgrey: "#d4d4d4",
+        medgrey: "#e0e0e0",
+        grey: "#f7f7f7",
+      },
+    },
   },
   plugins: [],
-}
+};
