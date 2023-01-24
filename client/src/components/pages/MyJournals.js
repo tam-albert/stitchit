ea72361a3688fcd46cc1542a8a3e425a302e5622
angelnuz <angelnuz@mit.edu>
2023-01-23 21:46:21 -0500

    help

--- client/src/components/pages/MyJournals.js
+++ client/src/components/pages/MyJournals.js
@@ -16,10 +16,12 @@ const MyJournals = () => {
   }, []);
 
   const journalsList = journals.map((journal) => (
-    <div className="test border-top-width-200 inline-flex border-2 border-gray-400 rounded-md p-4 my-4">
-      <Link to={`/journal/${journal._id}`} key={journal._id}>
-        {journal.name} <span className="text-gray-500">by {journal.collaborator_names[0]}</span>
-      </Link>
+    <div className="box">
+      <div className="inline-flex border-2 border-gray-400 rounded-md p-4 my-4">
+        <Link to={`/journal/${journal._id}`} key={journal._id}>
+          {journal.name} <span className="text-gray-500">by {journal.collaborator_names[0]}</span>
+        </Link>
+      </div>
     </div>
   ));
 
