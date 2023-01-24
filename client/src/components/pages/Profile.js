0248ea36f8f0c77e93c3cbbc17c40106a3e074a5
connieej <110947840+connieej@users.noreply.github.com>
2023-01-23 22:40:00 -0500

    profile changes part 1

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import "../../utilities.css";
 import "./Profile.css";
 
-const Profile = () => {
+const Profile = (props) => {
   useEffect(() => {
     document.title = "Profile Page";
   }, []);
@@ -13,12 +13,12 @@ const Profile = () => {
       <div className="Profile-avatarContainer">
         <div className="Profile-avatar" />
       </div>
-      <h1 className="Profile-name u-textCenter">Connie Jiang</h1>
+      <h1 className="Profile-name u-textCenter">{props.Name}</h1>
       <hr className="Profile-line" />
       <div className="u-flex">
         <div className="Profile-subContainer u-textCenter">
           <h4 className="Profile-subTitle">About Me</h4>
-          <div id="profile-description">hoe</div>
+          <div id="profile-description">insert bio</div>
         </div>
       </div>
     </>
