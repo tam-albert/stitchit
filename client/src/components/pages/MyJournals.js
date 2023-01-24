f4ab3ce1a1bbd1f315fc3fe7a4902e6682703598
Albert Tam <tamalbert8@gmail.com>
2023-01-23 22:03:07 -0500

    u can click on the cards now

--- client/src/components/pages/MyJournals.js
+++ client/src/components/pages/MyJournals.js
@@ -17,12 +17,12 @@ const MyJournals = () => {
 
   const journalsList = journals.map((journal) => (
     <div className="test flex flex-col border-2 border-gray-400 rounded-md my-4">
-      <div className="bg-gray-800 w-full h-48"></div>
-      <div className="m-4">
-        <Link to={`/journal/${journal._id}`} key={journal._id}>
+      <Link to={`/journal/${journal._id}`} key={journal._id}>
+        <div className="bg-gray-800 w-full h-48"></div>
+        <div className="m-4">
           {journal.name} <span className="text-gray-500">by {journal.collaborator_names[0]}</span>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   ));
 
