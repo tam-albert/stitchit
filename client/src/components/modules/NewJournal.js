9a53c3dae9a4e3edf08d6491df164bbf0735e65a
Albert Tam <tamalbert8@gmail.com>
2023-01-27 21:14:52 -0500

    make animation for making a new journal

--- client/src/components/modules/NewJournal.js
+++ client/src/components/modules/NewJournal.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+import { Transition } from "@headlessui/react";
 import { post } from "../../utilities";
 import { useNavigate } from "@reach/router";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
+
+import "./NewJournal.css";
 
 const NewJournal = () => {
   const [journalName, setJournalName] = useState("");
+  const [boxVisible, setBoxVisible] = useState(false);
 
   const navigate = useNavigate();
 
@@ -19,18 +24,39 @@ const NewJournal = () => {
     setJournalName("");
   };
 
+  const openPrompt = () => {
+    setBoxVisible(true);
+  };
+
+  const closePrompt = () => {
+    setBoxVisible(false);
+  };
+
   return (
-    <div className="u-flex">
-      <input
-        type="text"
-        placeholder="Journal name"
-        maxLength="50"
-        value={journalName}
-        className="pl-2"
-        onChange={handleChange}
-      />
-      <button type="submit" className="NewInput-button" value="Submit" onClick={handleSubmit}>
-        Create New Journal
+    <div className="flex justify-center items-center">
+      {boxVisible ? (
+        <>
+          <button onClick={closePrompt}>
+            <XMarkIcon className="w-8 h-8 mr-3 text-secondary" />
+          </button>
+          <input
+            type="text"
+            value={journalName}
+            onChange={handleChange}
+            placeholder="Enter journal name..."
+            maxLength="50"
+            className={`${
+              journalName.length ? "" : "italic"
+            } text-lg w-96 h-full pl-2 py-2 NewJournal-textbox`}
+          />
+        </>
+      ) : null}
+      <button
+        className="flex items-center ml-3 rounded-full px-4 py-2 border border-secondary text-secondary duration-100 hover:bg-secondary hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        onClick={boxVisible ? handleSubmit : openPrompt}
+      >
+        <PlusIcon className="w-6 h-6" />
+        <span className="ml-3 text-lg whitespace-nowrap">New Journal</span>
       </button>
     </div>
   );
