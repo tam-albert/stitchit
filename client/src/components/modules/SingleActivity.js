353b02f9613cac094bbaa429f8c73dbe9326f1a3
Albert Tam <tamalbert8@gmail.com>
2023-01-29 21:39:02 -0500

    make the whole div a link

--- client/src/components/modules/SingleActivity.js
+++ client/src/components/modules/SingleActivity.js
@@ -4,21 +4,17 @@ import { formatRelative } from "date-fns";
 
 const SingleActivity = (props) => {
   return (
-    <div className="p-4 border-2 border-neutral-500 rounded-md flex space-x-2 items-center">
-      {props.link ? (
-        <Link to={props.link}>
+    <Link to={props.link ? props.link : "#"}>
+      <div className="p-4 border-2 border-neutral-500 rounded-md flex space-x-2 items-center">
+        <div className="grow">
           <span className="font-bold">
             {props.selfId === props.activityId ? "You" : props.name}
           </span>{" "}
           {props.content}
-        </Link>
-      ) : (
-        <p className="grow">
-          <span className="font-bold">{props.name}</span> {props.content}
-        </p>
-      )}
-      <p className="text-gray-500">{formatRelative(new Date(props.timestamp), new Date())}</p>
-    </div>
+        </div>
+        <p className="text-gray-500">{formatRelative(new Date(props.timestamp), new Date())}</p>
+      </div>
+    </Link>
   );
 };
 
